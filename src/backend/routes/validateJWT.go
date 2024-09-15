f052b434069ece22fb87b04d6ec68241bf17f62b
reishimanfr <eryk.sarelo@icloud.com>
2024-09-15 14:11:02 +0200

    Added missing route file

---
+++ src/backend/routes/validateJWT.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) validateJWT(c *gin.Context) {
+	// The reason we can do this is that the JWT middleware
+	// already handles everything for us and writing the same
+	// code again would just be a waste of time
+	c.JSON(http.StatusOK, gin.H{
+		"message": "JWT token is valid",
+	})
+}
